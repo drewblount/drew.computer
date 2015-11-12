6b4d894554dfef3112d5023a0f18e1e172ab23be
drewblount <me@drew.computer>
2015-11-11 22:53:12 -0800

    some cleaning--about to try a beautification project

--- app.js
+++ app.js
@@ -3,3 +3,9 @@ $('.sidebar-stick').affix({
         top: 15
       }
 });
+
+/* changes active link status */
+$('li > a').click(function() {
+    $('li.active').removeClass('active');
+    $(this).parent().addClass('active');
+});
